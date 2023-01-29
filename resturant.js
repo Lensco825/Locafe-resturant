0330ba98a228490c5bdbd07069ce0c576dda0cf9
Lens <Lensdorsainvil711@outlook.com>
2023-01-29 18:49:36 -0500

    first commit

---
+++ resturant.js
@@ -0,0 +1,55 @@
+var html = document.querySelector('html');
+ var pageSize = html.getBoundingClientRect().width;
+ var home = document.querySelector('.home');
+ var courses = document.querySelector('.courses');
+
+ console.log(pageSize);
+
+ var darkToggle = document.querySelector('.darkMode');
+
+ darkToggle.addEventListener('click', function() {
+  home.classList.toggle('dark');
+  courses.classList.toggle('dark');
+  cursor.classList.toggle('cursor_dark');
+  cursor.classList.toggle('text-cursor_dark');
+ })
+
+ var cursor = document.querySelector('.cursor_1');
+ var cursor_two = document.querySelector('.cursor_2');
+
+document.addEventListener('mousemove', moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+  cursor.style.left = `${x}px`;
+  cursor.style.top = `${y}px`;
+  cursor_two.style.left = `${x}px`;
+  cursor_two.style.top = `${y}px`;
+}
+
+var text = document.querySelectorAll('p');
+var listItems = document.querySelectorAll('li');
+
+text.forEach(text => {
+  text.addEventListener('mousemove', function () {
+    cursor.classList.add('text-cursor');
+    cursor_two.classList.add('text-cursor_2');
+  })
+  text.addEventListener('mouseleave', function () {
+    cursor.classList.remove('text-cursor');
+    cursor_two.classList.remove('text-cursor_2');
+  })
+})
+
+
+listItems.forEach(listItems => {
+  listItems.addEventListener('mousemove', function () {
+    cursor.classList.add('text-cursor');
+    cursor_two.classList.add('text-cursor_2');
+  })
+  listItems.addEventListener('mouseleave', function () {
+    cursor.classList.remove('text-cursor');
+    cursor_two.classList.remove('text-cursor_2');
+  })
+})
